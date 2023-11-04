8830ce532a58b58b7e4198271837e7373c46e921
Milena Tyagra <milenatyagra.sc@gmail.com>
2023-11-04 18:08:32 -0300

    Criação de componente Content

---
+++ src/components/layout/Content.jsx
@@ -0,0 +1,21 @@
+import "./Content.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "../../views/examples/Home";
+import About from "../../views/examples/About";
+import Param from "../../views/examples/Param";
+import NotFound from "../../views/examples/NotFound";
+
+export default function Content() {
+  return (
+    <main className="Content">
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/param/:id" exact element={<Param />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </main>
+  );
+}
