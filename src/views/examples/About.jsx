cc7256ba1969053ea2c5586674aa393da5732d39
Milena Tyagra <milenatyagra.sc@gmail.com>
2023-11-04 18:05:57 -0300

    Criação de componente About

---
+++ src/views/examples/About.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="About">
+      <h1>Componente About</h1>
+      <h2>Sistema Criado!</h2>
+    </div>
+  );
+}
