2e3ac488a8ea1a2bdd6af37903dbffc362879c88
Milena Tyagra <milenatyagra.sc@gmail.com>
2023-11-04 18:06:34 -0300

    Criação de componente NotFound

---
+++ src/views/examples/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NotFound() {
+    return (
+        <div className="NotFound">
+            <h1>404</h1>
+            <h2>Opss... Página Não encontrada!</h2>
+        </div>
+    )
+}
